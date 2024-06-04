b8b499afdc2bbd3516eee793dad9594598d41380
lemagnyt <lemagnythomas@gmail.com>
2024-06-04 13:09:52 +0200

    add weapons

---
+++ Assets/Scripts/Combat/AssaultRifle.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+// Implémentation de la classe AssaultRifle
+[Serializable]
+public class AssaultRifle : Weapon
+{
+    public AssaultRifle(GameObject weaponPrefab, GameObject bulletPrefab, int nMunitionMax, int nMunition, float damage, string name, float maxDistance, float rangeDistance)
+        : base(weaponPrefab, bulletPrefab, nMunitionMax, nMunition, damage, name, maxDistance, rangeDistance)
+    {
+    }
+
+    public override float GetDamage(float distance)
+    {
+       
+    }
+
+    public override float GetRange()
+    {
+        // Implémentation vide pour le moment
+        return 0f;
+    }
+}
--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,23 +1,47 @@
 using UnityEngine;
 using System;
-[Serializable]
-public class Weapon
-{
-    public GameObject weaponPrefab;
-    public GameObject bulletPrefab;
-    public int nMunitionMax;
-    public int nMunition;
-    public float damage;
-    public string name;
 
-    // Constructeur de la classe Weapon
-    public Weapon(GameObject weaponPrefab, GameObject bulletPrefab, int nMunitionMax, int nMunition, float damage, string name)
+// Classe de base WeaponBase
+[Serializable]
+public abstract class Weapon
+{
+    public GameObject WeaponPrefab { get; set; }
+    public GameObject BulletPrefab { get; set; }
+    public int NMunitionMax { get; set; }
+    public int NMunition { get; set; }
+    public float Damage { get; set; }
+    public string Name { get; set; }
+    public float MaxDistance { get; set; }
+    public float RangeDistance { get; set; } // Distance à partir de laquelle les dégâts d'une munitions diminuent
+    private bool canShoot;
+    
+    public void Start()
     {
-        this.weaponPrefab = weaponPrefab;
-        this.bulletPrefab = bulletPrefab;
-        this.nMunitionMax = nMunitionMax;
-        this.nMunition = nMunition;
-        this.damage = damage;
-        this.name = name;
+        Initialize();
     }
+
+    public void Initialize()
+    {
+        canShoot = true;
+    }
+
+    public void Reload()
+    {
+        NMunition = NMunitionMax;
+    }
+
+    public Weapon(GameObject weaponPrefab, GameObject bulletPrefab, int nMunitionMax, int nMunition, float damage, string name, float maxDistance, float rangeDistance)
+    {
+        WeaponPrefab = weaponPrefab;
+        BulletPrefab = bulletPrefab;
+        NMunitionMax = nMunitionMax;
+        NMunition = nMunition;
+        Damage = damage;
+        Name = name;
+        RangeDistance = rangeDistance;
+        MaxDistance = maxDistance;
+    }
+
+    public abstract float GetDamage(float distance);
+    public abstract float GetRange();
 }
